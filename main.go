a1808cddfab682cfc0c3db195ead6e533da8f759
shanart <shanin.x@gmail.com>
2023-01-21 12:20:14 +0200

    base structure

---
+++ main.go
@@ -0,0 +1,41 @@
+package main
+
+import (
+	"fyne.io/fyne/v2"
+	"fyne.io/fyne/v2/app"
+	"fyne.io/fyne/v2/container"
+	"fyne.io/fyne/v2/widget"
+)
+
+type config struct {
+	EditWidget    *widget.Entry
+	PreviewWidget *widget.RichText
+	CurrentFile   fyne.URI
+	SaveMenuItem  *fyne.MenuItem
+}
+
+var cfg config
+
+func main() {
+	a := app.New()
+
+	win := a.NewWindow("Markdown")
+	edit, preview := cfg.makeUI()
+
+	win.SetContent(container.NewHSplit(edit, preview))
+
+	win.Resize(fyne.Size{Width: 800, Height: 640})
+	win.CenterOnScreen()
+	win.ShowAndRun()
+}
+
+func (app *config) makeUI() (*widget.Entry, *widget.RichText) {
+	edit := widget.NewMultiLineEntry()
+	preview := widget.NewRichTextFromMarkdown("")
+
+	app.EditWidget = edit
+	app.PreviewWidget = preview
+	edit.OnChanged = preview.ParseMarkdown
+
+	return edit, preview
+}
